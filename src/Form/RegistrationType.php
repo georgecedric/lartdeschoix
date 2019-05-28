209835f2d158d15bf151fd942cb11cb4d4eadb6f
georgecedric <george.cedric@hotmail.com>
2019-05-28 09:17:57 +0200

    validation du formulaire

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -7,15 +7,37 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('username')
-            ->add('password')
-            ->add('confirm_password')
+            ->add('username',  TextType::class, [
+                'attr' => [
+                    'placeholder' => "Veuillez entrer votre nom d'utilisateur"],
+                    'label' => "votre Pseudo"    
+                    ])
+            ->add('email',  EmailType::class, [
+                'attr' => [
+                     'placeholder' => "Veuillez entrer votre email"],
+                     'label' => "votre email"    
+                     ])
+            
+            ->add('password', PasswordType::class, [
+                'attr' => [
+                     'placeholder' => "Veuillez entrer votre mot de passe"],
+                     'label' => "votre mot de passe"    
+                     ])
+            ->add('confirm_password', PasswordType::class, [
+                'attr' => [
+                     'placeholder' => "Veuillez entrer une nouvelle fois votre mot de passe"],
+                     'label' => "confirmez votre mot de passe"    
+                     ])
         ;
     }
 
