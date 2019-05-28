344f322f9e9225ef70e6642baba9c3b60ebba641
georgecedric <george.cedric@hotmail.com>
2019-05-28 10:34:05 +0200

    hashage des mots de passes

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,12 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -78,4 +79,13 @@ class User
 
         return $this;
     }
+
+    public function eraseCredentials() {}
+
+    public function getSalt() {}
+
+        public function getRoles(){
+            return ['ROLE_USER'];
+        }
+
 }
